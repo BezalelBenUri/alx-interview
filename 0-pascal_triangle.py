b826a4936ae3efaa11762011605128f1bb385261
BezalelBenUri <diamondinyang@gmail.com>
2024-01-04 20:32:07 +0000

    paschal's triangle

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+A module for working with paschals's triangle
+"""
+def pascal_triangle(n):
+    """
+    Generate Pascal's triangle up to the nth row.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1] * (i + 1) for i in range(n)]
+
+    for i in range(2, n):
+        for j in range(1, i):
+            triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+
+    return triangle
