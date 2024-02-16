846f6e0d96878b81e9d88438d26efa66f48d5531
BezalelBenUri <diamondinyang@gmail.com>
2024-02-16 14:26:08 +0000

    0. Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,36 +1,39 @@
 #!/usr/bin/node
-const argv = process.argv;
-const urlFilm = 'https://swapi-api.hbtn.io/api/films/';
-const urlMovie = `${urlFilm}${argv[2]}/`;
 
+// Import the 'request' library
 const request = require('request');
 
-request(urlMovie, function (error, response, body) {
-  if (error == null) {
-    const fbody = JSON.parse(body);
-    const characters = fbody.characters;
+// Define constant with the base URL of the Star Wars API
+const API_URL = 'https://swapi-api.alx-tools.com/api';
 
-    if (characters && characters.length > 0) {
-      const limit = characters.length;
-      CharRequest(0, characters[0], characters, limit);
+// Check if the number of command line arguments is greater than 2
+if (process.argv.length > 2) {
+  // Make a request to the film resource for the specified film ID
+  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    // If an error occurred during the request, log the error
+    if (err) {
+      console.log(err);
     }
-  } else {
-    console.log(error);
-  }
-});
+    // Get the characters URL from the film's response body
+    const charactersURL = JSON.parse(body).characters;
 
-function CharRequest (idx, url, characters, limit) {
-  if (idx === limit) {
-    return;
-  }
-  request(url, function (error, response, body) {
-    if (!error) {
-      const rbody = JSON.parse(body);
-      console.log(rbody.name);
-      idx++;
-      CharRequest(idx, characters[idx], characters, limit);
-    } else {
-      console.error('error:', error);
-    }
+    // Create an array of Promises that resolve with the names of the characters
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        // Make a request to the character resource
+        request(url, (promiseErr, __, charactersReqBody) => {
+          // If an error occurred during the request, reject the Promise with the error
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          // Resolve the Promise with the name of the character
+          resolve(JSON.parse(charactersReqBody).name);
+        });
+      }));
+
+    // Wait for all Promises to resolve and log the names of the characters, separated by new lines
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
   });
 }
